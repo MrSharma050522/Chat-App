8da95c427184ba53fe5765306c0d6a49c3c4a2c7
Sandeep Sharma <macbook@MacBooks-MacBook-Pro.local>
2024-04-28 02:42:03 +0530

    Change to add rotues to user login jwt bcrypt

--- backend/Routes/ChatGroupRoute.js
+++ backend/Routes/ChatGroupRoute.js
@@ -3,17 +3,20 @@ const router = express.Router();
 const ChatGroup = require("../Modal/ChatGroupModal.js");
 const User = require("../Modal/UserModal.js");
 
-
 router.post("/createNewChatGroup", async (req, res) => {
     try {
         // console.log("Api HItted", req.body);
-        const {groupName, createdBy, users} = req.body;
+        const { groupName, createdBy, users } = req.body;
         // console.log("Name -> ", name);
-        const chatGroup = await new ChatGroup({ groupName: groupName, createdBy: createdBy, users: users });
-        for(let i=0; i<users.length; i++){
+        const chatGroup = await new ChatGroup({
+            groupName: groupName,
+            createdBy: createdBy,
+            users: users,
+        });
+        for (let i = 0; i < users.length; i++) {
             await User.findByIdAndUpdate(users[i], {
-                    $push: { chatGroups: chatGroup._id }
-                });
+                $push: { chatGroups: chatGroup._id },
+            });
         }
         chatGroup.save();
         console.log("Logging here", chatGroup);
@@ -23,19 +26,34 @@ router.post("/createNewChatGroup", async (req, res) => {
         console.log("Error -> ", error);
         res.status(400).json(error);
     }
-})
+});
 
 router.get("/getAllChatGroup", async (req, res) => {
     try {
-        console.log("Getting Here user");
+        // console.log("Getting Here user");
         const chatGroup = await ChatGroup.find().populate("createdBy users");
-        console.log("user", chatGroup);
+        // console.log("user", chatGroup);
 
         res.status(200).json(chatGroup);
     } catch (error) {
         console.log("Error -> ", error);
         res.status(400).json(error);
     }
-})
+});
+
+router.get("/getChatGroupDetailsById/:id", async (req, res) => {
+    try {
+        // console.log("Getting Here user");
+        const chatGroupDetail = await ChatGroup.findById({
+            _id: req.params.id,
+        }).populate("createdBy users");
+        // console.log("user", chatGroupDetail);
+
+        res.status(200).json(chatGroupDetail);
+    } catch (error) {
+        console.log("Error -> ", error);
+        res.status(400).json(error);
+    }
+});
 
 module.exports = router;
--- backend/Routes/UserRoute.js
+++ backend/Routes/UserRoute.js
@@ -1,28 +1,43 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../Modal/UserModal.js");
-
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 router.post("/createNewUser", async (req, res) => {
     try {
-        // console.log("Api HItted", req.body);
-        const {name, email, password} = req.body;
-        console.log("Name -> ", name);
-        const user = await new User({ name: name, email: email, password: password });
-        user.save();
-        console.log("Logging here", user);
+        const { name, email, password } = req.body;
+        const hashPassword = await bcrypt.hash(password, 10);
 
-        res.status(200).json(user);;
+        const emailExists = await User.findOne({ email: email });
+        if (emailExists) {
+            // console.log(emailExists);
+            return res.status(400).json("Email already Exists");
+        }
+
+        const user = await User.create({ name, email, password: hashPassword });
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, {
+            expiresIn: "3d",
+        });
+
+        user.password = "";
+
+        res.status(201).json({
+            user,
+            token,
+        });
     } catch (error) {
         console.log("Error -> ", error);
         res.status(400).json(error);
     }
-})
+});
 
 router.get("/getAllUser", async (req, res) => {
     try {
-        console.log("Getting Here user");
-        const users = await User.find().select("name email createdAt updatedAt chatGroups active");
+        // console.log("Getting Here user");
+        const users = await User.find().select(
+            "name email createdAt updatedAt chatGroups active"
+        );
         console.log("user", users);
 
         res.status(200).json(users);
@@ -30,6 +45,47 @@ router.get("/getAllUser", async (req, res) => {
         console.log("Error -> ", error);
         res.status(400).json(error);
     }
-})
+});
+
+router.post("/userLogin", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({
+                status: "fail",
+                message: "Email and Password should not be empty",
+            });
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({
+                message: "Email is not registered!",
+            });
+        }
+
+        const verify = await bcrypt.compare(password, user.password);
+        if (!verify) {
+            return res.status(400).json({
+                message: "Password is Incorrect!",
+            });
+        }
+
+        user.password = "";
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, {
+            expiresIn: "24h",
+        });
+        res.status(200).json({
+            status: "success",
+            user,
+            token,
+        });
+    } catch (err) {
+        res.status(500).json({
+            error: err.message,
+        });
+    }
+});
 
 module.exports = router;
